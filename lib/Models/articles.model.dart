c7388538b956c52222ed39954bfb2897fd403c8d
Andres  Rodriguez <36413311+rcandres@users.noreply.github.com>
2021-09-17 19:25:05 -0500

    feat: Modelo

---
+++ lib/Models/articles.model.dart
@@ -0,0 +1,21 @@
+class Article {
+
+String author;
+String title;
+String description;
+String url;
+String urlToImage;
+String publishedAt;
+String content;
+
+
+  Article({
+    required this.author,
+    required this.title,
+    required this.description,
+    required this.url,
+    required this.urlToImage,
+    required this.publishedAt,
+    required this.content
+  });
+}
